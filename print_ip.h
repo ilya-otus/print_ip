6d40057e4249afe3a118420e1e0faa84de3f8d6a
Ilya <podshivalov.ilya@yandex.ru>
2018-11-28 13:21:35 +0300

    Fix interoperabillity issue

--- print_ip.h
+++ print_ip.h
@@ -12,7 +12,7 @@ template<typename B1>
 struct conjunction<B1> : B1 {};
 
 template<typename B1, typename... Bn>
-struct conjunction<B1, Bn...> : std::conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
+struct conjunction<B1, Bn...> : std::conditional<bool(B1::value), conjunction<Bn...>, B1>::type {};
 
 template<typename T>
 struct is_container {
